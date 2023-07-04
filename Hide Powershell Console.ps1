b17b806fba32cd37f0aaa6862c2ddfd22b603c12
Marcus <136156702+markanlb@users.noreply.github.com>
2023-07-04 19:36:33 +0200

    Add files via upload

--- Hide Powershell Console.ps1
+++ Hide Powershell Console.ps1
@@ -1,4 +1,4 @@
-# Hide Powershell Console.ps1, v0.2, 2023
+# Hide Powershell Console.ps1, v0.1, 2023
 #
 # By Marcus
 #
@@ -6,12 +6,18 @@
 #==========================================
 $debug = $true # if true = console is visible
 
-$windowcode = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
-$asyncwindow = Add-Type -MemberDefinition $windowcode -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
-if ($debug -eq $true) {
-    $null = $asyncwindow::ShowWindowAsync((Get-Process -PID $pid).MainWindowHandle, 5) # 5 to Show console
-    Write-Host "DEBUG WINDOW" -ForegroundColor Cyan
-    Write-Host "Do not close this window; report any errors that appear during testing" -ForegroundColor Cyan
-} else {
-    $null = $asyncwindow::ShowWindowAsync((Get-Process -PID $pid).MainWindowHandle, 0) # 0 to Hide console
+Add-Type -AssemblyName PresentationCore, PresentationFramework, WindowsBase
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+    [DllImport("Kernel32.dll")]
+    public static extern IntPtr GetConsoleWindow();
+    
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+function Hide-Console
+{
+    $console = [Console.Window]::GetConsoleWindow()
+    [Console.Window]::ShowWindow($console, 0) | Out-Null
 }
+if ($debug -eq $false){
+	Hide-Console }
